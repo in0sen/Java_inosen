87a371d1272b9b5ad99dc3d5f5288129c4fa81f0
S19 <S19@MSI>
2023-09-19 10:17:52 +0900

    my first commit

--- JavaBase/src/Course58/Practice03.java
+++ JavaBase/src/Course58/Practice03.java
@@ -5,8 +5,8 @@ public class Practice03 {
 		JavaMaker jm = new JavaMaker();
 		String startCode = "import java.util.*;public class HelloWorld { public static void main(String[] args){"
 				+ "Random ra=new Random();"
-				+ "for(int a=0; a<10; a++){ System.out.println(ra.nextInt(1000));}"
-				+ ""
+				+ "Scanner stdIn=new Scanner(System.in);"
+				+ "for(int a=0; a<10; a++){ System.out.println(ra.nextInt(stdIn.nextInt()));}"
 				+ "}}";
 		jm.javaCodeStart(startCode);
 	}
